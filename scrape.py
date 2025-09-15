3f912e02b0062a2d9316c2ad9c902dffb65995a7
NeelimaPopuri <popurineelima5@gmail.com>
2025-09-15 12:56:53 -0700

    scrapping the data

---
+++ scrape.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def fetch_books(page_number):
+    url = f"https://books.toscrape.com/catalogue/page-{page_number}.html"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.prettify())
+
+
+def main():
+    fetch_books(1)
+
+
+if __name__ == "__main__":
+    main()
