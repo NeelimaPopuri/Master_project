a211d80e42f830aa1b56460af80af286fcb01ecd
NeelimaPopuri <popurineelima5@gmail.com>
2025-09-17 12:22:40 -0700

    counter

---
+++ counter.py
@@ -0,0 +1,19 @@
+class Counter:
+    def __int__(self):
+        self.value = 1
+
+    def count_up(self):
+        self.value += 1
+
+    def count_down(self):
+        self.value -= 1
+
+
+count1 = Counter()
+count2 = Counter()
+
+count1.count_up()
+count2.count_up()
+
+print(count1, count2)
+print(count1 + count2)
